a4e7d0e6a2c816787e94c4a91d26bf1cdf270f97
ts7rs <ts-7@yandex.ru>
2021-06-06 19:58:38 +0200

    fix: subtasks 1, 2 of task 4

--- Swift_Task4/ios.stage-task/Exercises/Exercise-1/IntToRoman.swift
+++ Swift_Task4/ios.stage-task/Exercises/Exercise-1/IntToRoman.swift
@@ -1,9 +1,12 @@
 import Foundation
 
 public extension Int {
-
+    
     var roman: String? {
     var integerValue = self
+        
+    if integerValue <= 0 || integerValue > 3999 { return nil }
+        
     var numeralString = ""
     let mappingList: [(Int, String)] = [(1000, "M"), (900, "CM"), (500, "D"), (400, "CD"), (100, "C"), (90, "XC"), (50, "L"), (40, "XL"), (10, "X"), (9, "IX"), (5, "V"), (4, "IV"), (1, "I")]
       for i in mappingList {
@@ -12,9 +15,7 @@ public extension Int {
           numeralString += i.1
         }
       }
-        if numeralString == ""{
-            return nil
-        }
+    if numeralString == "" { return nil }
     return numeralString
     }
 }
